3cd5cd388cccfbf7d33c9a48721e1cb796c96f29
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-08-07 12:21:24 +0500

    inner html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 /*alert("Hello World");*/
 /*prompt("Enter your name");*/
-document.write("<h1>Hello Word</h1>")
+/*document.write("<h1>Hello Word</h1>");*/
+document.getElementById("test").innerHTML="Hello World";
