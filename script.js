8f7fc999f39ab166a30075585374881b7951305d
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-08-08 19:51:28 +0500

    functions

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 /*alert("Hello World");*/
-var name=prompt("Enter your name");
-document.write("<h1>"+name+"</h1>");
-document.write(document.baseURI+"<br>");
-document.write(document.title+"<br>")
-console.log(name);
 /*document.getElementById('test').innerHTML = "Hello World";*/
 /*console.log("Hello World");*/
+function enterName(){
+    var name=prompt("Enter your name");
+    document.write("<h1>"+name+"</h1>");
+    document.write(document.baseURI+"<br>");
+    document.write(document.title+"<br>")
+    console.log(name);
+    return name;
+}
+var usernmae=enterName();
+document.write("<h2>"+usernmae+"</h2>");
