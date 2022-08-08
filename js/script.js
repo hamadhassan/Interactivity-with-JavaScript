a96947a1446957fd0377656373a186b3d7a1eb1c
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-08-08 21:07:51 +0500

    practice the functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*alert("Hello World");*/
 /*document.getElementById('test').innerHTML = "Hello World";*/
 /*console.log("Hello World");*/
-function enterName(){
+/*function enterName(){
     var name=prompt("Enter your name");
     document.write("<h1>"+name+"</h1>");
     document.write(document.baseURI+"<br>");
@@ -10,4 +10,10 @@ function enterName(){
     return name;
 }
 var usernmae=enterName();
-document.write("<h2>"+usernmae+"</h2>");
+document.write("<h2>"+usernmae+"</h2>");*/
+function first(){
+    document.getElementById("change").innerHTML="First Button is clicked";
+}
+function second(){
+document.getElementById("change").innerHTML="Second Button is Clicked";
+}
