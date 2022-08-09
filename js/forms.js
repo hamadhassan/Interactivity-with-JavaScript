5bbd12f5ba8595463cdf6cf21f56a50c7a62d91b
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-08-09 13:47:41 +0500

    checkbox and radio button

--- js/forms.js
+++ js/forms.js
@@ -8,3 +8,13 @@ function valpas(){
         alert("Email not matched");
     }
 }
+function nicknameFunction(){
+    if (yesNick.checked){
+  nick.style.display="inline";
+  nickname.setAttribute('required',true);
+    }
+    else{ 
+        nickname.removeAttribute('required');
+        nick.style.display="none";
+    }
+}
